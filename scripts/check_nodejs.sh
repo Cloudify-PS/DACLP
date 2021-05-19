820ddc529f704f7be05f3abb6b2f4bfb2f2374d0
Michał Mordawski <michal.mordawski@amartus.com>
2021-05-19 08:26:57 +0200

    day1 showcase

--- scripts/check_nodejs.sh
+++ scripts/check_nodejs.sh
@@ -1,5 +1,10 @@
-status=$(pm2 show app | grep status | awk '{print $4}')
-if [[ "$status" != "online" ]]; then
-  echo "pm2 returned wrong status: $status"
-  exit 1
+#!/bin/bash
+status_line=$(sudo su -c "pm2 show app | grep status")
+status=$(echo "$status_line" | awk '{print $4;}')
+if [[ "$status" == "online" ]]
+  then
+    echo "The app is running in pm2."
+  else
+    echo "pm2 returned wrong status: $status"
+    exit 1
 fi
