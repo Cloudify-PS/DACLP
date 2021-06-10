f101e849f0aade6b6a18804e9e3b455b0692c165
Michał Mordawski <michal.mordawski@amartus.com>
2021-06-10 11:16:50 +0200

    fix outputs/capabilities when scaled out

---
+++ scripts/runtime_properties_append_ip.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin python
+from cloudify import ctx
+from cloudify.state import ctx_parameters
+from cloudify.exceptions import NonRecoverableError
+
+runtime_properties = ctx_parameters['runtime_properties']
+try:
+    for p in runtime_properties:
+        n, v = map(p.get, ['name', 'value'])
+        ctx.target.instance.runtime_properties[n] = \
+            ctx.target.instance.runtime_properties.setdefault(n, [])[:]
+        ctx.target.instance.runtime_properties[n].append(v)
+        ctx.logger.info('source: {}'
+                        .format(ctx.source.instance.runtime_properties))
+        ctx.logger.info('target: {}'
+                        .format(ctx.target.instance.runtime_properties))
+except KeyError:
+    raise NonRecoverableError('Runtime properties to fetch must be a '\
+        'list of dictionaries with name and value pairs: {}'.format(runtime_properties))
